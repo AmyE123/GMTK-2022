196d4014d4b509a5b1ffe2f15d47a53411e0f753
JoePC <iam3djoe@gmail.com>
2022-07-17 16:51:34 +0100

    level select scroll

--- Assets/Scripts/LevelGrid.cs
+++ Assets/Scripts/LevelGrid.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using GameData;
+using UnityEngine.UI;
 
 public class LevelGrid : MonoBehaviour
 {
     [SerializeField] GameObject _buttonPrefab;
     [SerializeField] GameSettings _settings;
+    [SerializeField] private ScrollRect scrollRect;
 
     public void Start()
     {
@@ -17,6 +19,8 @@ public class LevelGrid : MonoBehaviour
             GameObject newObj = Instantiate(_buttonPrefab, transform);
             newObj.GetComponent<PlayLevelButton>().Init(lvl, i);
             i++;
-        }  
+        } 
+
+        scrollRect.normalizedPosition = Vector2.one;
     }
 }
